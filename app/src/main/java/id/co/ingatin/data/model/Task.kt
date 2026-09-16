0626fe4cfd074f62ec0d4df89d8c38eb21c4d6b4
Raqhib <raqhib.ataillah@gmail.com>
2026-09-16 15:52:17 +0800

    fix(model) : rename auth and mytask

--- app/src/main/java/id/co/ingatin/data/model/Task.kt
+++ app/src/main/java/id/co/ingatin/data/model/Task.kt
@@ -1,12 +1,13 @@
 package id.co.ingatin.data.model
 
+import com.google.firebase.Timestamp
 import com.google.firebase.firestore.DocumentId
 
 data class MyTask(
     @DocumentId
     val taskId: String = "",
     val category: String = "",
-//    val dueDate: String,
+    val dueDate: Timestamp,
     val title: String = "",
     val description: String = ""
 )
